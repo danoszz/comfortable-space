10548b7bfe024235758f20b4f824fdc6e2a67cda
Daan van der Zwaag <daan@devign.it>
2019-05-23 23:57:37 +0200

    add: helper functions

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -2,3 +2,12 @@
 export function toggleClass(listener, name) {
   return listener ? null : name
 }
+
+// Send email
+export function sendEmail(email, subject, message) {
+  window.location.href = `mailto: ${email}?subject=${subject}&body=${message}`
+}
+
+export function openURL(url) {
+  window.location.href = url
+}
